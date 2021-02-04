fe532d797b6f47ad311e8d481e92688102f61ea2
MongieLee <le30n@outlook.com>
2021-02-04 23:45:26 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Form {
+  sex: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-demo';
+  public formData: Form = {
+    sex: '',
+  };
+
+  getData = () => {
+    console.log(this.formData);
+  };
 }
